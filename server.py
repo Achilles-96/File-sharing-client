814d6ccd40ae712fb62d27d9428f93d7013425c8
erilyth <vishal.batchu@students.iiit.ac.in>
2016-02-10 10:30:25 +0530

    Base commit

---
+++ server.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 vishalapr <vishalapr@vishalapr-Lenovo-G50-70>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+# server.py
+
+import socket                   # Import socket module
+
+port = 60000                    # Reserve a port for your service.
+s = socket.socket()             # Create a socket object
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+host = socket.gethostname()     # Get local machine name
+s.bind((host, port))            # Bind to the port
+s.listen(5)                     # Now wait for client connection.
+
+print 'Server listening....'
+
+while True:
+    conn, addr = s.accept()     # Establish connection with client.
+    print 'Got connection from', addr
+    data = conn.recv(1024)
+    print('Server received', repr(data))
+
+    filename='mytext.txt'
+    f = open(filename,'rb')
+    l = f.read(1024)
+    while (l):
+       conn.send(l)
+       print('Sent ',repr(l))
+       l = f.read(1024)
+    f.close()
+
+    print('Done sending')
+    #conn.send('Thank you for connecting')
+    conn.close()
