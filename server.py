89244a0ed148a8b2c55ea7d4bcae8045d858b1cb
erilyth <vishal.batchu@students.iiit.ac.in>
2016-02-10 12:08:28 +0530

    Add basic OOP and file send/receive features

--- server.py
+++ server.py
@@ -2,36 +2,47 @@
 
 import socket                   # Import socket module
 
-port = 60000                    # Reserve a port for your service.
-s = socket.socket()             # Create a socket object
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-host = socket.gethostname()     # Get local machine name
-s.bind((host, port))            # Bind to the port
-s.listen(5)                     # Now wait for client connection.
+class Server:
 
-print 'Server listening....'
+    def init(self):
+        port = 60000                    # Reserve a port for your service.
+        s = socket.socket()             # Create a socket object
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        host = socket.gethostname()     # Get local machine name
+        s.bind((host, port))            # Bind to the port
+        s.listen(5)                     # Now wait for client connection.
+        return s
 
-while True:
-    conn, addr = s.accept()     # Establish connection with client.
-    print 'Got connection from', addr
-    data = conn.recv(1024)
-    print('Server received', repr(data))
+    def runServer(self):
+        s = self.init()
+        print 'Server listening....'
 
-    if data=="Hello server!":
-        conn.send("Hello client!")
+        while True:
+            conn, addr = s.accept()     # Establish connection with client.
+            print 'Got connection from', addr
+            data = conn.recv(1024)
+            print('Server received', repr(data))
 
-    if data=="File List":
-        conn.send("My file list")
+            if data=="Hello server!":
+                conn.send("Hello client!")
 
-    '''filename='mytext.txt'
-    f = open(filename,'rb')
-    l = f.read(1024)
-    while (l):
-       conn.send(l)
-       print('Sent ',repr(l))
-       l = f.read(1024)
-    f.close()
+            if data=="File List":
+                conn.send("My file list")
 
-    print('Done sending')
-    #conn.send('Thank you for connecting')'''
-    conn.close()
+            if "Select File" in data:
+                command,value = data.split(':')
+                filename='mytext.txt'
+                f = open(filename,'rb')
+                l = f.read(1024)
+                while (l):
+                    conn.send(l)
+                    print('Sent ',repr(l))
+                    l = f.read(1024)
+                    f.close()
+                print('Done sending')
+                #conn.send('Thank you for connecting')'''
+            
+            conn.close()
+
+server = Server()
+server.runServer()
